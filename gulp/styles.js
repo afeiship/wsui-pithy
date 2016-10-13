566c45fc1bb5da6e540c6529e22dc7a419eea5a5
feizheng <1290657123@qq.com>
2016-10-13 22:02:27 +0800

    inital & finish

---
+++ gulp/styles.js
@@ -0,0 +1,24 @@
+(function() {
+
+  'use strict';
+
+  var gulp = require('gulp');
+  var config = require('./config');
+  var argv = require('yargs').argv;
+  var $ = require('gulp-load-plugins')({
+    pattern: ['gulp-*', 'gulp.*', 'del']
+  });
+
+  //styles
+  gulp.task('styles', function() {
+    return gulp.src('src/*.scss')
+      .pipe($.concat('webkit-css-modules.scss'))
+      .pipe(gulp.dest('dist'))
+      .pipe($.sass(config.sassOptions.normal).on('error', $.sass.logError))
+      .pipe(gulp.dest('dist'))
+      .pipe($.sass(config.sassOptions.minify).on('error', $.sass.logError))
+      .pipe($.rename({extname:'.min.css'}))
+      .pipe(gulp.dest('dist'));
+  });
+
+}());
