7567d7848cc75a5e6447bc11f2c99d235e60d256
afeiship <1290657123@qq.com>
2023-06-08 06:50:49 +0800

    feat: add weapp styles

--- build/clean.js
+++ build/clean.js
@@ -1,12 +1,11 @@
-(function() {
-  'use strict';
-  var gulp = require('gulp');
-  var $ = require('gulp-load-plugins')({
-    pattern: ['gulp-*', 'gulp.*', 'del'],
-  });
+'use strict';
 
-  //clean
-  gulp.task('clean', function() {
-    return $.del('dist');
-  });
-})();
+var gulp = require('gulp');
+var $ = require('gulp-load-plugins')({
+  pattern: ['gulp-*', 'gulp.*', 'del'],
+});
+
+//clean
+gulp.task('clean', function () {
+  return $.del('dist');
+});
--- build/styles.js
+++ build/styles.js
@@ -1,21 +1,38 @@
-(function () {
-  'use strict';
+'use strict';
 
-  var gulp = require('gulp');
-  var autoprefixer = require('autoprefixer');
-  var $ = require('gulp-load-plugins')({
-    pattern: ['gulp-*', 'gulp.*', 'del', '@jswork/gulp-*'],
-  });
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const $ = require('gulp-load-plugins')({
+  pattern: ['gulp-*', 'gulp.*', 'del', '@jswork/gulp-*'],
+});
 
-  //styles
-  gulp.task('styles', function () {
-    return gulp
-      .src('src/*.scss')
-      .pipe($.concat('index.scss'))
-      .pipe($.jswork.pkgHeader())
-      .pipe(gulp.dest('dist'))
-      .pipe($.sass({ outputStyle: 'expanded' }).on('error', $.sass.logError))
-      .pipe($.rename('style.css'))
-      .pipe(gulp.dest('dist'));
-  });
-})();
+//styles
+gulp.task('styles:web', function () {
+  return gulp
+    .src('src/*.scss')
+    .pipe($.concat('index.scss'))
+    .pipe($.jswork.pkgHeader())
+    .pipe(gulp.dest('dist'))
+    .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
+    .pipe($.rename('style.css'))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('styles:weapp', function () {
+  return gulp
+    .src('src/*.scss')
+    .pipe($.concat('index.scss'))
+    .pipe($.replace(/&_ > \*:first-child/g, '&_ > view:first-child, &_ > first:last-child'))
+    .pipe($.replace(/&_ > \*:last-child/g, '&_ > view:last-child, &_ > text:last-child'))
+    .pipe($.replace(/> \*:first-child/g, '> view:first-child, > text:first-child'))
+    .pipe($.replace(/> \*:last-child/g, '> view:last-child, > text:last-child'))
+    .pipe($.replace(/&_ > \*/g, '&_ > view, &_ > text'))
+    .pipe($.replace(/& > \*/g, '& > view, & > text'))
+    .pipe($.rename('weapp.index.scss'))
+    .pipe(gulp.dest('dist'))
+    .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
+    .pipe($.rename('weapp.style.css'))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('styles', gulp.series(['styles:web', 'styles:weapp']));
